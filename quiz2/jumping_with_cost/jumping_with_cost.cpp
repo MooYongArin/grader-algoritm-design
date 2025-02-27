83c94db3bd140c74c0ae1e9939a6dff60a759871
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-02-27 11:08:41 +0700

    manage_quiz_2

--- quiz2/jumping_with_cost/jumping_with_cost.cpp
+++ quiz2/jumping_with_cost/jumping_with_cost.cpp
@@ -7,17 +7,6 @@ int main(){
     for(int i = 1; i <= n; i++) cin >> a[i];
     for(int i = 1; i <= k; i++) cin >> b[i];
 
-    if(k == 1) {
-        fill(dp, dp + n, INT_MIN);
-        dp[0] = 0;
-        dp[1] = a[1];
-        for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + a[i] - b[1];
-        }
-
-        cout << dp[n] << endl;
-        return 0;
-    }
     dp[0] = 0;
     dp[1] = a[1];
     for(int i = 2; i <= n; i++) {
