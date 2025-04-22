8af4f20235d955dd8e153edcda806b598e938f6c
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-04-22 23:07:40 +0700

    last_before_quiz

--- 04-graph/shortest_path_in_hex_map/hex_map.cpp
+++ 04-graph/shortest_path_in_hex_map/hex_map.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
-
+#define tt tuple<int,int,int> // cost,row,col
 using namespace std;
-int main(){
-    
-    return 0;
+int r,c,a1,a2,b1,b2,m[303][303],used[303][303];
+int dr[6]={0,0,-1,-1,1,1};
+int dce[6]={-1,1,-1,0,-1,0};
+int dco[6]={-1,1,0,1,0,1};
+
+int main() {
+    cin>>r>>c>>a1>>b1>>a2>>b2;
+    for (int i=1;i<=r;i++) {
+        for (int j=1;j<=c;j++) {
+            cin>>m[i][j];
+        }
+    }
+    priority_queue<tt,vector<tt>,greater<tt>> q;
+    q.emplace(m[a1][b1],a1,b1);
+    while (!q.empty()) {
+        auto [z,row,col]=q.top();
+        q.pop();
+        if (used[row][col]) continue;
+        used[row][col]=z;
+        for (int i=0;i<6;i++) {
+            int nrow=row+dr[i];
+            int ncol=(row%2)?col+dco[i]:col+dce[i];
+            if (1<=nrow&&nrow<=r&&1<=ncol&&ncol<=c&&!used[nrow][ncol]) {
+                q.emplace(z+m[nrow][ncol],nrow,ncol);
+            }
+        }
+    }
+    cout<<used[a2][b2]<<"\n";
 }
--- 05-state_space_search/clique/clique.cpp
+++ 05-state_space_search/clique/clique.cpp
@@ -1,8 +1,39 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int main(){
+int n, sum_pow = 0;
+int f[50][50], b[58], max_pow = INT_MIN;
+vector<int> v;
+bool isClose(){
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = i + 1; j < v.size(); j++)
+        {
+            if(f[v[i]][v[j]] != 1 || f[v[j]][v[i]] != 1) return false;
+        }
+    }
+    return true;
+}
+void recur(int idx, int power, int sum_pow){
+    if(idx == n) {
+        max_pow = max(max_pow, power);
+        return;
+    }
+    if(power + sum_pow < max_pow) return;
     
+    v.push_back(idx);
+    if(isClose()) recur(idx + 1, power + b[idx], sum_pow - b[idx]);
+
+    v.pop_back();
+    recur(idx + 1, power, sum_pow - b[idx]);
+}
+int main(){
+    cin >> n;
+    for (int i = 0; i < n; i++) {cin >> b[i]; sum_pow += b[i];}
+
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) {cin >> f[i][j];}
+
+    recur(0, 0, sum_pow);
+    cout << max_pow << '\n';
     return 0;
 }
