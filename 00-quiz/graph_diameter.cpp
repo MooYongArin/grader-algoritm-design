22c0c243cb0edcf5629952d2221cdf95caec5c71
MooYongArin <yongariyata@gmail.com>
2025-05-01 09:31:23 +0700

    graph_diameter

---
+++ 00-quiz/graph_diameter.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int w[n][n], dist[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) w[i][j] = 0;
+            else w[i][j] = INT_MAX;
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        --a; --b;
+        w[a][b] = c;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            dist[i][j] = w[i][j];
+        }
+        
+    }
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++) {
+                if(dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) continue;
+                dist[i][j] = min (dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    int max_path = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (dist[i][j] == INT_MAX) continue;
+            max_path = max(max_path, dist[i][j]);
+        }
+    }
+    cout << max_path << '\n';
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,29 +1,71 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-stack<int> s;
-int cc_num = 0;
-void dfs1(int idx, vector<vector<int>> &adj, vector<int> &visited){
-    visited[idx] = 0;
-    for (auto &b : adj[idx])
-    {
-        if(visited[b] != -1) continue;
-        // visited[b] = true;
-        dfs1(b, adj, visited);
-    }
-    
-    s.push(idx);
-}
-void dfs2(int idx, vector<vector<int>> &adj, vector<int> &visited){
-    visited[idx] = cc_num;
-    for (auto &b : adj[idx])
-    {
-        if(visited[b] != -1) continue;
-        dfs2(b, adj, visited);
-    }
-}
-int main() {
-    
+struct State {
+    int space; 
+    int last_space;
+    vector<int> cur_state;
+    vector<int> combi;
+};
 
+int main() {
+    int n, m;
+    cin >> n >> m; // 0 -> red, 1 -> blue, 2 -> space
+    vector<vector<int>> adj(2 * n + 1);
+    set<vector<int>> same;
+    vector<int> ans, start;
+
+    for (int i = 0; i < n; i++) ans.push_back(1);
+    for (int i = n; i < 2 * n; i++) ans.push_back(0);
+    ans.push_back(2);
+
+    for (int i = 0; i < n; i++) start.push_back(0);
+    for (int i = n; i < 2 * n; i++) start.push_back(1);
+    start.push_back(2);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        --a;
+        --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    stack<State> q;
+    q.push({2 * n, -1, start, {}});
+    same.insert(start);
+
+    while (!q.empty()) {
+        State current = q.top();
+        q.pop();
+
+        int space = current.space;
+        vector<int> cur_state = current.cur_state;
+        vector<int> combi = current.combi;
+
+        for (int b : adj[space]) {
+            swap(cur_state[b], cur_state[space]);
+
+            if (same.find(cur_state) == same.end() && current.last_space != b) { // NOT FOUND
+                combi.push_back(b + 1);
+                if (cur_state == ans) {
+                    cout << combi.size() << '\n';
+                    for (auto &i : combi) {
+                        cout << i << " ";
+                    }
+                    return 0;
+                }
+                q.push({b, space, cur_state, combi});
+                same.insert(cur_state);
+                combi.pop_back();
+            }
+
+            swap(cur_state[b], cur_state[space]);
+        }
+    }
+
+    cout << -1 << '\n';
     return 0;
 }
